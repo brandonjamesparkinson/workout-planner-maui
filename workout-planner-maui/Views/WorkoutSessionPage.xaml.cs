a62110e7d29b7fe92332a7d237f4ad50854f1114
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2026-02-19 21:36:45 +0000

    reworking ui styling changes

--- workout-planner-maui/Views/WorkoutSessionPage.xaml.cs
+++ workout-planner-maui/Views/WorkoutSessionPage.xaml.cs
@@ -1,4 +1,6 @@
+using workout_planner_maui.Models;
 using workout_planner_maui.ViewModels;
+using Microsoft.Maui.Devices;
 
 namespace workout_planner_maui.Views;
 
@@ -9,4 +11,30 @@ public partial class WorkoutSessionPage : ContentPage
 		InitializeComponent();
 		BindingContext = new WorkoutSessionViewModel();
 	}
+
+    private async void OnRepTapped(object sender, EventArgs e)
+    {
+        if (sender is VisualElement element && element.BindingContext is ExerciseSet set)
+        {
+            // Micro-Interaction: Scale Animation
+            await element.ScaleTo(0.95, 100, Easing.CubicOut);
+            await element.ScaleTo(1.0, 100, Easing.CubicIn);
+
+            // Haptic Feedback
+            try 
+            {
+                HapticFeedback.Default.Perform(HapticFeedbackType.Click);
+            }
+            catch (Exception)
+            {
+                // Ignore if not supported
+            }
+
+            // Execute Logic
+            if (set.CycleRepsCommand.CanExecute(null))
+            {
+                set.CycleRepsCommand.Execute(null);
+            }
+        }
+    }
 }
